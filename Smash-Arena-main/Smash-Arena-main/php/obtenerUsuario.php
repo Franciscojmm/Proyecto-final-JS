1dc3aca114d4dc806dfb53f345184a7fb38012a0
Franciscojmm <61140966+Franciscojmm@users.noreply.github.com>
2022-02-23 18:12:50 +0100

    Delete Smash-Arena-main directory

--- Smash-Arena-main/Smash-Arena-main/php/obtenerUsuario.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_cache_limiter();
-session_start();
-
-
-echo json_encode($_SESSION); 
-?>
--- Smash-Arena-main/formularios/frmModificarUsuario.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('comboUsuarios').addEventListener("change",mostrarDatosUsuario,false);
-frmModificarUsuario.botonEnviar.addEventListener("click",modificarUsuario,false);
-recuperarUsuarios();
